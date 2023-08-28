4cd65bbf493bad38e559b472a373e66ae5f85d99
yuvala <yuv.almaliah@gmail.com>
2023-08-28 23:45:58 +0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import { itemsRouter } from './items/items.router'
+dotenv.config()
+
+/**
+ * App Variables
+ */
+
+if (!process.env.PORT) {
+    process.exit(1)
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
+const app = express()
+
+/**
+ *  App Configuration
+ */
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use('/api/menu/items', itemsRouter)
+
+/**curl http://localhost:7000/api/menu/items -i
+ * Server Activation
+ */
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
